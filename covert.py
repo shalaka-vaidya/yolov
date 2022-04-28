8e19ac28750cc29fe49c4a89108c4c793bb60231
Shalaka Vaidya <shalakavaidya@b-10-27-88-6.dynapool.vpn.nyu.edu>
2022-04-28 15:55:08 -0400

    convert

--- covert.py
+++ covert.py
@@ -1,5 +1,8 @@
 # Function to get the data from XML Annotation
 import xml.etree.ElementTree as ET
+import os 
+from tqdm import tqdm
+
 def extract_info_from_xml(xml_file):
     root = ET.parse(xml_file).getroot()
     
@@ -34,4 +37,54 @@ def extract_info_from_xml(xml_file):
             info_dict['bboxes'].append(bbox)
     
     return info_dict
+
 print(extract_info_from_xml('Road_Sign_Dataset/annotations/road4.xml'))
+
+# Dictionary that maps class names to IDs
+class_name_to_id_mapping = {"trafficlight": 0,
+                           "stop": 1,
+                           "speedlimit": 2,
+                           "crosswalk": 3}
+
+# Convert the info dict to the required yolo format and write it to disk
+def convert_to_yolov5(info_dict):
+    print_buffer = []
+    
+    # For each bounding box
+    for b in info_dict["bboxes"]:
+        try:
+            class_id = class_name_to_id_mapping[b["class"]]
+        except KeyError:
+            print("Invalid Class. Must be one from ", class_name_to_id_mapping.keys())
+        
+        # Transform the bbox co-ordinates as per the format required by YOLO v5
+        b_center_x = (b["xmin"] + b["xmax"]) / 2 
+        b_center_y = (b["ymin"] + b["ymax"]) / 2
+        b_width    = (b["xmax"] - b["xmin"])
+        b_height   = (b["ymax"] - b["ymin"])
+        
+        # Normalise the co-ordinates by the dimensions of the image
+        image_w, image_h, image_c = info_dict["image_size"]  
+        b_center_x /= image_w 
+        b_center_y /= image_h 
+        b_width    /= image_w 
+        b_height   /= image_h 
+        
+        #Write the bbox details to the file 
+        print_buffer.append("{} {:.3f} {:.3f} {:.3f} {:.3f}".format(class_id, b_center_x, b_center_y, b_width, b_height))
+        
+    # Name of the file which we have to save 
+    save_file_name = os.path.join("Road_Sign_Dataset/annotations/", info_dict["filename"].replace("png", "txt"))
+    
+    # Save the annotation to disk
+    print("\n".join(print_buffer), file= open(save_file_name, "w"))
+
+# Get the annotations
+annotations = [os.path.join('Road_Sign_Dataset/annotations/', x) for x in os.listdir('Road_Sign_Dataset/annotations/') if x[-3:] == "xml"]
+annotations.sort()
+
+# Convert and save the annotations
+for ann in tqdm(annotations):
+    info_dict = extract_info_from_xml(ann)
+    convert_to_yolov5(info_dict)
+annotations = [os.path.join('Road_Sign_Dataset/annotations/', x) for x in os.listdir('Road_Sign_Dataset/annotations/') if x[-3:] == "txt"]
